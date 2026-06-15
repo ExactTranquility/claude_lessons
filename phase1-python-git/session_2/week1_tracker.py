fd31e5f3628e3138ecb0e2758b0bd5d6c8bf4829
Steven Szatkiewicz <stevensza.work@gmail.com>
2026-06-15 06:12:27 -0400

    clean the text input and formatting for week1_tracker.py

--- phase1-python-git/session_2/week1_tracker.py
+++ phase1-python-git/session_2/week1_tracker.py
@@ -1,53 +1,91 @@
+from typing import Callable, TypedDict
+from collections.abc import Mapping
+
+class MenuEntry(TypedDict):
+    label: str
+    command: Callable[[], None]
+
+MenuDict = Mapping[str, MenuEntry]
+
+
+def clean_text(text: str) -> str:
+    return " ".join(text.strip().split()).capitalize()
 
 
 def add_item() ->  None:
-    pass
+    print("Adding")
 
 
 def view_items() -> None:
-    pass
+    print("Viewing")
     
     
 def remove_item() -> None:
-    pass
+    print("Removing")
     
     
 def count_items() -> None:
-    pass
-    
-    
-def quit() -> None:
-    pass
+    print("Counting")
     
 
-def get_safe_input(prompt: str) -> str:
+def get_safe_input(prompt: str) -> str | None:
     while True:
-        user_input = input(prompt)
-        if user_input:
-            return user_input
-        print("Invalid input, please try again")
+        user_input = clean_text(input(prompt))
+        if not user_input:
+            return None
+        return user_input
 
 
-# menu item, callable
-menu = {
-    "1" : {"label" : "Add item", "command" : add_item},
-    "2" : {"label" : "View items", "command" : view_items},
-    "3" : {"label" : "Remove item", "command" : remove_item},
-    "4" : {"label" : "Count items", "command" : count_items},
-    "5" : {"label" : "Quit", "command" : quit}
-}
-
-
-# : dict[index: int, callable()]
-def show_menu(menu) -> None:
-    for key, entry in menu.items():
-        print(f"{key}.) {entry['label']}")
-        
+def show_menu(menu: MenuDict) -> None:    
+    print()
+    for index, value in menu.items():
+        print(f"{index}.) {value['label']}")
+    print()
 
 
 
 def main() -> None:
-    show_menu(menu)
+    running = True
+    
+    def exit_program() -> None:
+        nonlocal running
+        running = False
+    
+    menu: MenuDict = {
+    "1" : {
+        "label" : "Add item",
+        "command" : add_item
+    },
+    "2" : {
+        "label" : "View items",
+        "command" : view_items
+    },
+    "3" : {
+        "label" : "Remove item",
+        "command" : remove_item
+    },
+    "4" : {
+        "label" : "Count items",
+        "command" : count_items
+    },
+    "5" : {
+        "label" : "Quit",
+        "command" : exit_program
+    }
+    }
+    
+    while running:
+        show_menu(menu)
+        user_input = get_safe_input("Choose a menu option: ")        
+        
+        if user_input is None:
+            print("Input cannot be blank")
+            continue
+
+        if user_input in menu:
+            menu[user_input]['command']()
+        else:
+            print("Invalid menu option, please try again")
 
 
 if __name__ == "__main__":
