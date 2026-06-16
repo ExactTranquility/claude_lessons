a4720ce81c16435168b5d9ddf2807b91e2be5dcd
Steven Szatkiewicz <stevensza.work@gmail.com>
2026-06-16 16:43:56 -0400

    git not longer tracks .json files. Load and save contacts functions added. get_clean_text renamed to get_safe_input for accuracy

--- phase1-python-git/session_3/contact_book.py
+++ phase1-python-git/session_3/contact_book.py
@@ -1,3 +1,15 @@
+### TODO
+# # load and save contacts to a persistent JSON storage
+# save function
+# load function
+
+# # Add contacts
+# List contacts
+# find contact
+# remove contact
+
+import json
+from pathlib import Path
 
 from typing import Callable, TypedDict
 from collections.abc import Mapping
@@ -9,17 +21,31 @@ class MenuEntry(TypedDict):
 
 MenuDict = Mapping[str, MenuEntry]
 
-def save_contacts():
-    pass
 
-def load_contacts():
-    pass
+#  Learned from phase1 workbook - tried finding docs breifly but could only find general stack
+# overflow questions and answers that confirmed usage
+script_dir = Path(__file__).resolve().parent # needs additional learning
+SAVE_FILE = script_dir / "contacts.json"
+
+def save_contacts(contact_list: list, path: str) -> None:
+    with open(path, 'w') as f:
+        json.dump(contact_list, f, indent=2)
+
+def load_contacts(path: str) -> list:
+    try:
+        with open(path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return [] # first run - no file - return empty object
+    except json.JSONDecodeError:
+        print("Warning: File could not be read, loading blank contacts.")
+        return [] # return empty contact list object if file reads corrupt 
 
 def clean_text(text: str) -> str:
-    return " ".join(text.split()).strip().title()
+    return " ".join(text.split()).title()
 
 
-def get_clean_text(prompt: str) -> str | None:
+def get_safe_input(prompt: str) -> str | None:
     user_input = clean_text(input(prompt))
     if not user_input:
         return None
@@ -27,7 +53,7 @@ def get_clean_text(prompt: str) -> str | None:
 
 def get_non_empty(prompt: str) -> str:
     while True:
-        user_input = get_clean_text(prompt)
+        user_input = get_safe_input(prompt)
         if user_input is None:
             print("Entry was empty, please try again.")
             continue
@@ -36,8 +62,8 @@ def get_non_empty(prompt: str) -> str:
 
 def show_menu(menu: MenuDict):
     print()
-    for i, menu_item in menu.items():
-        print(f"{i}.) {menu_item['label']}")
+    for i, entry in menu.items():
+        print(f"{i}.) {entry['label']}")
     print()
 
 
@@ -53,6 +79,7 @@ def handle_menu_input(prompt: str, menu: MenuDict) -> None:
 
 def add_contact():
     pass
+            
 
 def list_contact():
     pass
@@ -65,6 +92,10 @@ def delete_contact():
 
 def main() -> None:
     running = True
+    contacts = load_contacts(SAVE_FILE)
+    contacts.append({"name" : "Steven", "Number" : "(863) 656 - 6411"})
+    print(contacts)
+    save_contacts(contacts, SAVE_FILE)
 
     def exit_program() -> None:
         nonlocal running
