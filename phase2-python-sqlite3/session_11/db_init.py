ad27bb7c2434c9fce8a3102c71046fc563e59e5b
ExactTranquility <stevensza.work@gmail.com>
2026-06-26 08:54:59 -0400

    wrapped table creation into init function

--- phase2-python-sqlite3/session_11/db_init.py
+++ phase2-python-sqlite3/session_11/db_init.py
@@ -10,27 +10,33 @@ def execute_db(database: Path, command: str) -> None:
         conn.execute(command)
         conn.commit()
 
-execute_db(characters_db, """
-    CREATE TABLE IF NOT EXISTS users(
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL)
-""")
-
-execute_db(characters_db, """
-        CREATE TABLE IF NOT EXISTS characters(
-            char_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            name TEXT NOT NULL UNIQUE,
-            level INTEGER NOT NULL DEFAULT 1,
-            FOREIGN KEY (user_id) REFERENCES users (user_id))
-""")
-
-execute_db(characters_db, """
-        CREATE TABLE IF NOT EXISTS weapons(
-            weap_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            level INTEGER NOT NULL DEFAULT 1,
-            refinment INTEGER NOT NULL DEFAULT 1,
-            FOREIGN KEY (user_id) REFERENCES users (user_id))
-           """)
+def init_characters_db() -> None:
+    # create users table so multiple people can use the database w/ login credentials
+    execute_db(characters_db, """
+        CREATE TABLE IF NOT EXISTS users(
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL)
+    """)
+    # create character table
+    execute_db(characters_db, """
+            CREATE TABLE IF NOT EXISTS characters(
+                char_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                name TEXT NOT NULL UNIQUE,
+                level INTEGER NOT NULL DEFAULT 1,
+                FOREIGN KEY (user_id) REFERENCES users (user_id))
+    """)
+    # create weapon table
+    execute_db(characters_db, """
+            CREATE TABLE IF NOT EXISTS weapons(
+                weap_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                level INTEGER NOT NULL DEFAULT 1,
+                refinment INTEGER NOT NULL DEFAULT 1,
+                FOREIGN KEY (user_id) REFERENCES users (user_id))
+            """)
+    
+if __name__ == "__main__":
+    for _ in range(0, 3):
+        init_characters_db()
