4166d5fe39f7c3ddd9c71aa7c8f45c39f2daf8c3
ExactTranquility <stevensza.work@gmail.com>
2026-06-27 16:01:41 -0400

    initial commit session_12, database creates and pass statements along desired functions

---
+++ phase2-python-sqlite3/session_12/db_crud.py
@@ -0,0 +1,79 @@
+ # # # TODO
+ 
+from pathlib import Path
+import sqlite3
+
+absolute_path = Path(__file__).parent
+master_database = absolute_path / 'characters.db'
+
+def connect_execute_db_close(path: Path, command: str) -> None:
+    with sqlite3.connect(path) as conn:
+        conn.execute(command)
+        conn.commit()
+
+
+def init(path: Path) -> None:
+    connect_execute_db_close(path, """
+                            CREATE TABLE IF NOT EXISTS users(
+                                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                username TEXT UNIQUE NOT NULL,
+                                password TEXT NOT NULL
+                            )"""
+    )
+    connect_execute_db_close(path, """
+                            CREATE TABLE IF NOT EXISTS characters(
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                user_id INTEGER NOT NULL,
+                                name TEXT UNIQUE NOT NULL,
+                                level INTEGER NOT NULL DEFAULT 0,
+                                auto_level INTEGER NOT NULL DEFAULT 1,
+                                skill_level INTEGER NOT NULL DEFAULT 1,
+                                burst_level INTEGER NOT NULL DEFAULT 1,
+                                constellations INTEGER NOT NULL DEFAULT 1,
+                                FOREIGN KEY (user_id) REFERENCES users (user_id)
+                            )"""
+    )
+    connect_execute_db_close(path,"""
+                            CREATE TABLE IF NOT EXISTS weapons(
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                user_id INTEGER NOT NULL,
+                                level INTEGER NOT NULL DEFAULT 1,
+                                refinement INTEGER NOT NULL DEFAULT 1,
+                                FOREIGN KEY (user_id) REFERENCES users (user_id)
+                            )"""
+    
+    )
+
+
+
+def insert_user() -> None:
+    pass
+
+
+def get_user_by_username() -> None:
+    pass
+
+
+def get_user_by_id() -> None:
+    pass 
+
+
+def get_all_users() -> None:
+    pass
+
+
+def insert_character() -> None:
+    pass
+
+
+def get_characters_for_user() -> None:
+    pass
+
+
+
+def main() -> None:
+    init(master_database) 
+
+
+if __name__ == "__main__":
+    main()
